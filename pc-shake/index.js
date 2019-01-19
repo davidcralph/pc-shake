46157cf838217388d30b687d293271a697e3c3ae
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-19 19:52:06 +0000

    Add files via upload

---
+++ pc-shake/index.js
@@ -0,0 +1,27 @@
+const Plugin  = require('powercord/Plugin');
+const webpack = require('powercord/webpack');
+
+module.exports = class qtradio extends Plugin {
+    start() {
+        powercord
+            .pluginManager
+            .get('pc-commands')
+            .register(
+                'shake',
+                'I don\'t know what this is',
+                'shake <number>',
+                async (number) => {
+                    return webpack.getModule(['ComponentDispatch']).ComponentDispatch.dispatch(webpack.constants.ComponentActions.SHAKE_APP, {
+                        duration: 10000,
+                        intensity: number
+                    });
+                }
+            )
+    }
+    unload() {
+        powercord
+            .pluginManager
+            .get('pc-commands')
+            .unregister('shake');
+    }
+};
