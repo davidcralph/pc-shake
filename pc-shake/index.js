b219cbeb0568e510c98914296c21343ce80ee37b
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-19 20:09:31 +0000

    oops

--- pc-shake/index.js
+++ pc-shake/index.js
@@ -1,7 +1,7 @@
 const Plugin  = require('powercord/Plugin');
 const webpack = require('powercord/webpack');
 
-module.exports = class qtradio extends Plugin {
+module.exports = class shake extends Plugin {
     start() {
         powercord
             .pluginManager
